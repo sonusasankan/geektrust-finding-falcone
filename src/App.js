2758eec87699ab47f83719cd20bf9bc607a9369c
Ajith Chandran <ajithchandran@Ajiths-MacBook-Air.local>
2019-02-10 17:55:45 +0530

    Implemented vehicle selection

--- src/App.js
+++ src/App.js
@@ -4,29 +4,138 @@ import "./App.css";
 import FormContainer from "./components/container/FormContainer";
 import Select from "./components/presentational/Select"
 class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
 
     this.state = {
-        values: [],
+      planets: [],
+      selected: [],
+      selects: {
+        select1: {
+          value: 'Select a planet',
+        },
+        select0: {
+          value: 'Select a planet',
+        },
+        select2: {
+          value: 'Select a planet',
+        },
+        select3: {
+          value: 'Select a planet',
+        },
+        select4: {
+          value: 'Select a planet',
+
+        },
+        select5: {
+          value: 'Select a planet',
+        },
+      },
+      current: '',
+      selectedVehicles: [],
+      vehicle: ''
     }
 
     this.addPlanets = this.addPlanets.bind(this);
+    this._handleChange = this._handleChange.bind(this);
+    this._selectVehicle = this._selectVehicle.bind(this);
+    // this._renderSelect = this._renderSelect.bind(this)
 
-  }  
+  }
 
-  addPlanets(e){
-    let obj = [{
-      [e.target.name] : e.target.value,
-      "speed": e.target.speed 
-    }]
+  //fetching planets//
+  componentDidMount() {
+    Promise.all([
+      fetch('https://findfalcone.herokuapp.com/planets'),
+      fetch('https://findfalcone.herokuapp.com/vehicles')
+    ])
+      .then(([res1, res2]) => Promise.all([res1.json(), res2.json()]))
+      .then(([data1, data2]) => this.setState({
+        planets: data1,
+        vehicles: data2
+      }));
+  }
 
-    this.setState((state, props)=>({
-       values: obj
+  addPlanets(e) {
+    // let distance = 100
+    let obj = this.state.planets.find(el => {
+      return el.name === e.target.value
+    })
+    console.log(filtered)
+    this.setState((state, props) => ({
+      selected: {
+        ...state.selected,
+        [name]: obj
+      }
     }))
   }
 
+  _handleChange(e) {
+    let obj = this.state.planets.find(el => {
+      return el.name === e.target.value
+    })
+
+    let toFilter = this.state.planets;
+
+    let filtered = toFilter.filter(item => {
+      return item.name.indexOf(e.target.value) == -1
+    })
+
+    let prevState = this.state.selects;
+    if (e.target.value !== "Select a planet") {
+      this.setState({
+        selects: {
+          ...prevState,
+          [e.target.name]: {
+            value: e.target.value,
+            distance: obj.distance
+          }
+        },
+        planets: filtered,
+      })
+    } else {
+      this.setState({
+        selects: {
+          ...prevState,
+          [e.target.name]: {
+            value: e.target.value,
+          }
+        },
+      })
+    }
+  }
+
+  _selectVehicle(e, i) {
+    let target = e.target;
+    this.setState((prevState) => ({
+      selects: {
+        ...prevState.selects,
+        [`select${i}`]: {
+          ...prevState.selects.select0,
+          vehicle: target.value,
+          speed: 100
+        }
+      }
+    }))
+  }
+
+  // _renderSelect({ planets, vehicles, selects, selectedVehicles } = this.state) {
+  //   while (i < 4) {
+  //     return (
+  //       <Select
+  //         selectedVehicles={selectedVehicles}
+  //         selectVehicle={(e) => this._selectVehicle(e, index)}
+  //         vehicle={this.state.vehicle}
+  //         value={selects[`select${index}`].value}
+  //         id={index} name={`select${index}`}
+  //         options={planets} vehicles={vehicles}
+  //         onChange={this._handleChange} />
+  //     )
+  //   }
+  // }
+
   render() {
+    const { planets, vehicles, selects, selectedVehicles } = this.state;
     return (
       <main className="main">
         <div className="gf-section">
@@ -35,18 +144,31 @@ class App extends Component {
               <h1 className="gf-title-main">Finding Falcone!</h1>
             </div>
             <div className="row-fluid text-center py-4">
-            <h3 className="gf-title-sub">Select planets you want to search in:</h3>
+              <h3 className="gf-title-sub">Select planets you want to search in:</h3>
             </div>
             <div className="row py-4">
-                <div className="col-md-3">
-                    <Select speed="100" name={`select${1}`} value={this.state.value} onChange={this.addPlanets} options={['Donlon', 'Enchai', 'Jebing', 'Sapir', 'Lerbin', 'Pingasar']}/>
-                    {this.state.value != ""? <h3>radio button</h3>: ""}
-                </div>
-
+              {
+                planets.map((planet, index) => {
+                return (
+                  index < 4 ?
+                    <div className="col-md-3">
+                      <Select
+                        selectedVehicles={selectedVehicles}
+                        selectVehicle={(e) => this._selectVehicle(e, index)}
+                        vehicle={this.state.vehicle}
+                        value={selects[`select${index}`].value}
+                        id={index} name={`select${index}`}
+                        options={planets} vehicles={vehicles}
+                        onChange={this._handleChange} />
+                    </div> : null
+                )
+              })
+              
+              }
             </div>
             <div className="row-fluid text-center">
-             <h4>Time taken: 4hr</h4>
-             <button className="btn btn-primary">Find falcon</button>
+              <h4>Time taken: 4hr</h4>
+              <button className="btn btn-primary">Find falcon</button>
             </div>
           </div>
         </div>
--- src/components/presentational/Select.jsx
+++ src/components/presentational/Select.jsx
@@ -1,18 +1,43 @@
-import React from "react";
+import React, {Component} from "react";
 import PropTypes from "prop-types";
 
-const Select = ({options, onChange, value, name, speed}) => {
-  return(
-    <div className="ff-select">
-    <select speed={speed} name={name} value={value} onChange={onChange}>
-      {options.map(option => {
-          return(
-            <option value={option}>{option}</option>
-          )
-      })}  
-    </select>
-  </div>
-  )
+class Select extends Component {
+  constructor(props){
+    super(props);
+  }
+
+
+  render() {
+    const { options, id, name, value, onChange, vehicles, selectVehicle} = this.props
+    return (
+      <div className="ff-select">
+        <select name={name} value={value} onChange={onChange}>
+          {options.map((option,i) => {
+            return (
+              i === 0? <option value='Select a planet'>Select a planet</option>:
+              <option value={option.name}>{option.name}</option>
+            )
+          })}
+        </select>
+        <div className="form-check">
+          
+          {
+            vehicles.map((vehicle , i) => {
+              return (
+                value != "Select a planet"?
+                <React.Fragment>
+                  <input onChange={selectVehicle} className="form-check-input" type="radio" name={`vehicle${id}`} id={vehicle.name.replace(/ /g,'') + id} value={vehicle.name} />
+                  <label className="form-check-label" htmlFor={vehicle.name.replace(/ /g,'') + id}>
+                    {vehicle.name}
+                  </label><br></br>
+                </React.Fragment>: null
+              )
+            })
+          }
+        </div>
+      </div>
+    )
+  }
 };
 
 export default Select;
